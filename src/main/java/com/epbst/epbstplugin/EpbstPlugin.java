ce76cc2d433f07e28f341816cd6a82ab2df925ca
FishHave <104718849+FishHave@users.noreply.github.com>
2023-05-01 15:28:32 +0800

    Add files

---
+++ src/main/java/com/epbst/epbstplugin/EpbstPlugin.java
@@ -0,0 +1,16 @@
+package com.epbst.epbstplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public final class EpbstPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
